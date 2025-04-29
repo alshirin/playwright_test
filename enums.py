e6ffdc8710798c39187e48e3e2a0568759876c39
alshirin <alexey.shirin.ge@gmail.com>
2025-04-30 02:06:16 +0400

    add ENUMs for radio and checkbox for main page request form

--- enums.py
+++ enums.py
@@ -1,7 +1,18 @@
-from enum import Enum
+from enum import Enum, auto
 
 
 class BrowserType(Enum):
     CHROMIUM = "chromium"
     FIREFOX = "firefox"
     SAFARI = "webkit"
+
+
+class AccountPurpose(Enum):
+    BUSINESS = "Business"
+    PERSONAL = "Personal"
+
+
+class WithdrawOption(Enum):
+    CASH = "Cash"
+    CARD = "Card"
+    CRYPTO = "Cryptocurrency"
